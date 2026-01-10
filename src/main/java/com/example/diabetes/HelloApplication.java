e6e2779af47b69fd87f35f690a9623a238304748
aaddiibb <adibraian43@gmail.com>
2026-01-10 13:25:39 +0600

    logo added

--- src/main/java/com/example/diabetes/HelloApplication.java
+++ src/main/java/com/example/diabetes/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.diabetes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -11,6 +12,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws Exception {
         // ✅ Initialize DB once at app start
         DatabaseManager.getInstance().initialize();
+
+        // Set application icon
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("logo.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("Could not load application icon: " + e.getMessage());
+        }
 
         FXMLLoader fxmlLoader = new FXMLLoader(
                 HelloApplication.class.getResource("role_selection.fxml")
